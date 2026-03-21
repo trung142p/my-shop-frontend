526ea1f45c8909a7d32acdb22896ab5fa64d3104
trung142p <trung142p@gmail.com>
2026-03-21 18:45:51 +0700

    fix language

--- src/i18n.js
+++ src/i18n.js
@@ -2,7 +2,6 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// Import các file dịch
 import viCommon from './locales/vi/common.json';
 import viHome from './locales/vi/home.json';
 import viProduct from './locales/vi/product.json';
